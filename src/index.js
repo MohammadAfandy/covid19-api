015655198fe9e12396b6bf39eaa7913cb5067a4d
Mohammad Afandy <afandy9895@gmail.com>
2021-06-03 19:34:09 +0700

    refractor function and error response data

--- src/index.js
+++ src/index.js
@@ -128,13 +128,13 @@ app.get('/cache', async (req, res, next) => {
 
 // not found handler
 app.use((req, res, next) => {
-  res.status(404).send('Not Found')
+  res.status(404).json({ message: 'Not Found' })
 });
 
 // error handler
 app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).send(err.message || 'Internal Server Error')
+  res.status(500).json({ message: err.message || 'Internal Server Error' })
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
